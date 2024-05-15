26b0c5b51342a06f7c441c2d0794a28feda52c28
Robin COURAULT <robin.courault@etu.univ-grenoble-alpes.fr>
2024-05-15 17:19:07 +0200

    Restylisation de la page

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 function switchPage(pageName) {
     let firstPageParcoured = document.querySelector('section');
 
-    while(firstPageParcoured.nodeName !== "FOOTER" && firstPageParcoured != null) {
+    while(firstPageParcoured != null && firstPageParcoured.nodeName !== "FOOTER") {
         if (firstPageParcoured.id === pageName+"_page") {
             firstPageParcoured.hidden = false;
+            document.getElementById(firstPageParcoured.id+"-btn").setAttribute("class", "selected");
         } else {
+            document.getElementById(firstPageParcoured.id+"-btn").removeAttribute("class");
             firstPageParcoured.hidden = true;
         }
 
